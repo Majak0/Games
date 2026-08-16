53c9c4f94a9c8ffd60b1b5a90c9e81f3367e7ec5
Majak <jacquot.m25@gmail.com>
2026-08-16 15:10:05 +0200

    ajout favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <title>{{ config('app.name') }}</title>
+        <link rel="icon" href="{{ asset('assets/favicon.ico') }}" type="image/x-icon">
+        <link rel="shortcut icon" href="{{ asset('assets/favicon.ico') }}" type="image/x-icon">
         @fonts
         @vite(['resources/css/app.css', 'resources/js/app.ts'])
     </head>
